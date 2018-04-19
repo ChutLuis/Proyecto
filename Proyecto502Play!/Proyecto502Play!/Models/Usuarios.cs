527cc4049cc021242c35b1963a1b84f2ba2dd8ac
ricardochianc <ricardochianc@hotmail.com>
2018-04-18 20:14:10 -0600

    Nuevo proyecto que tiene, login, admin, carga de usuarios

---
+++ Proyecto502Play!/Proyecto502Play!/Models/Usuarios.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto502Play_.Models
+{
+    public class Usuarios
+    {
+        public IList<UserModel> ListadoUsuarios { get; set; }
+    }
+}
---
+++ Proyecto502Play!/Proyecto502Play!/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto502Play_.Startup))]
+namespace Proyecto502Play_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
